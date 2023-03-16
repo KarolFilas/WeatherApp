90a53f177c197749e9a9a2c61bbbb680c3d882aa
Karol Filas <filaskarol@gmail.com>
2023-03-16 19:58:51 +0100

    default London current weather function

--- script.js
+++ script.js
@@ -6,3 +6,25 @@ const photo = document.querySelector('.photo')
 const weather = document.querySelector('.weather')
 const temperature = document.querySelector('.temperature')
 const humidity = document.querySelector('.humidity')
+
+const API_LINK = 'https://api.openweathermap.org/data/2.5/weather?q=';
+const API_KEY = '&appid=c8b373a64adad70fae2af1909b0fee87';
+const API_UNITS = '&units=metric';
+
+const getWeather = () => {
+    const city = input.value || 'London'
+    const URL = API_LINK + city + API_KEY + API_UNITS
+
+    axios.get(URL).then(res => {
+        console.log(res.data);
+        const temp = res.data.main.temp
+        const hum = res.data.main.humidity
+
+        cityName.textContent = res.data.name
+
+        temperature.textContent = Math.floor(temp) + 'oC'
+        humidity.textContent = hum + '%'
+    })
+}
+
+getWeather()
