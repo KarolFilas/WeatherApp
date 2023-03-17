6b3d5d278758248ebf07e748ae17a990c6c0e19b
Karol Filas <filaskarol@gmail.com>
2023-03-17 10:23:53 +0100

    final commit

--- script.js
+++ script.js
@@ -12,19 +12,49 @@ const API_KEY = '&appid=c8b373a64adad70fae2af1909b0fee87';
 const API_UNITS = '&units=metric';
 
 const getWeather = () => {
-    const city = input.value || 'London'
+    const city = input.value || 'Warsaw'
     const URL = API_LINK + city + API_KEY + API_UNITS
 
     axios.get(URL).then(res => {
         console.log(res.data);
         const temp = res.data.main.temp
         const hum = res.data.main.humidity
+        const status = Object.assign({}, ...res.data.weather)
+        console.log(status.id);
+
+        warning.textContent = ''
+        input.value = ''
+
+        if (status.id >= 200 && status.id <= 232) {
+            photo.setAttribute('src', './img/thunderstorm.png')
+        } else if (status.id >= 300 && status.id <= 321) {
+            photo.setAttribute('src', './img/drizzle.png')
+        } else if (status.id >= 500 && status.id <= 531) {
+            photo.setAttribute('src', './img/rain.png')
+        } else if (status.id >= 600 && status.id <= 622) {
+            photo.setAttribute('src', './img/snow.png')
+        } else if (status.id >= 701 && status.id <= 781) {
+            photo.setAttribute('src', './img/unknown.png')
+        } else if (status.id === 800) {
+            photo.setAttribute('src', './img/sun.png')
+        } else if (status.id >= 801 && status.id <= 804) {
+            photo.setAttribute('src', './img/cloud.png')
+        }
+
+        // photo.setAttribute('src', './img/thunderstorm.png')
 
         cityName.textContent = res.data.name
-
-        temperature.textContent = Math.floor(temp) + 'oC'
+        weather.textContent = status.main
+        temperature.textContent = Math.floor(temp) + '°C'
         humidity.textContent = hum + '%'
-    })
+    }).catch(() => warning.textContent = 'Wpisz poprawną nazwę miasta!')
 }
 
-getWeather()
+const enterCheck = (e) => {
+    if (e.key === 'Enter') {
+        getWeather()
+    }
+}
+
+input.addEventListener('keyup', enterCheck)
+button.addEventListener('click', getWeather)
